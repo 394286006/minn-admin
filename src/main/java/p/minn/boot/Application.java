ecd5cd932672a804d6799bbe9cef9c6f35a7e152
394286006 <freemanfreelift@gmail.com>
2017-12-20 10:08:55 +0800

    add spring boot

--- src/main/java/p/minn/boot/Application.java
+++ src/main/java/p/minn/boot/Application.java
@@ -3,30 +3,34 @@ package p.minn.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 /**
  * 
  * @author minn 
  * @QQ:3942986006
  * 
  */
-/*
+
 @Configuration
 @SpringBootApplication
 @EnableAutoConfiguration
-@ComponentScan
 @ImportResource({"classpath*:/spring/spring-mvc.xml"
 		,"classpath*:/spring/applicationContext-privilege.xml"
 		,"classpath*:/spring/applicationContext-workflow.xml"
 		,"classpath*:/spring/applicationContext-hadoopspark.xml"
 		,"classpath*:/spring/applicationContext-spring-security.xml"
 		,"classpath*:/spring/ignite/applicationContext-ignite.xml"})
-*/
-public class Application /*extends SpringBootServletInitializer*/ {
+
+public class Application extends SpringBootServletInitializer{
 	public static void main(String[] args) {
-//	    SpringApplication.run(Application.class, args);
+	    SpringApplication.run(Application.class, args);
 	  }
 }
