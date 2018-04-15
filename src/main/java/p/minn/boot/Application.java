7728027b9e42f270533e13147baebb6bd570b145
394286006 <freemanfreelift@gmail.com>
2018-04-15 17:35:38 +0800

    zuul

--- src/main/java/p/minn/boot/Application.java
+++ src/main/java/p/minn/boot/Application.java
@@ -3,7 +3,12 @@ package p.minn.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.client.RestTemplate;
 /**
  * 
  * @author minn 
@@ -11,7 +16,8 @@ import org.springframework.context.annotation.ImportResource;
  * 
  */
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
+@EnableEurekaClient
 //@EnableHystrix
 //@EnableHystrixDashboard
 @ImportResource({"classpath*:/spring/spring-mvc.xml"
@@ -25,5 +31,9 @@ public class Application extends SpringBootServletInitializer{
 	public static void main(String[] args) {
 	    SpringApplication.run(Application.class, args);
 	  }
-
+	@Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
